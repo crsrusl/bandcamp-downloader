c0f339c6b584ba3a72cf64f3866062b9fbefc60f
Chris Russell <chris.russell@havas.com>
2020-05-18 13:09:07 +0100

    Add artwork into ID3

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ axios.get(artistPage, {responseType: 'text'}).then(function (response) {
     const data = body.split('trackinfo: ')[1].split('}],')[0] + '}]';
     const parsed = JSON.parse(data);
     const albumTitle = body.split('album_title: ')[1].split(',')[0].split('"')[1];
+    const artID = 'a' + body.split('art_id: ')[1].split(',')[0] + '_16';
+    const artworkURL =  'https://f4.bcbits.com/img/' + artID;
     const artist = body.split('artist: ')[1].split(',')[0].split('"')[1];
     const albumName = `./downloads/${artist.replace(/\//g, '-')} - ${albumTitle.replace(/\//g, '-')}`;
 
@@ -23,43 +25,66 @@ axios.get(artistPage, {responseType: 'text'}).then(function (response) {
           url: item.file["mp3-128"],
           album: albumTitle,
           artist: artist,
-          trackNumber: item.track_num
+          trackNumber: item.track_num,
+          artworkURL: artworkURL
         }
     });
 
     if (!fs.existsSync(albumName)){
       fs.mkdirSync(`./downloads/${artist.replace(/\//g, '-')} - ${albumTitle.replace(/\//g, '-')}`);
+      
+      axios({method: 'get', url: artworkURL, responseType: 'stream'}).then(artwork => {
+        let write = fs.createWriteStream(`./${albumName}/${artID}.jpeg`);
+        artwork.data.pipe(write);
+
+        write.on('finish', () => {
+          fs.readFile(`./${albumName}/${artID}.jpeg`, function(err, data) {
+            if (err) throw err;
+            getTracks(data)
+          });
+        });
+      }).catch(err => console.log(err));
     }
 
-    format.forEach(function (item) {
-      if (item)
-        axios({method: 'get', url: item.url, responseType: 'stream'})
-        .then(function (response) {
-          let write = fs.createWriteStream(`./${albumName}/${item.title}.mp3`);
-
-          response.data.pipe(write);
-
-          write.on('finish', () => {
-            let file = `./${albumName}/${item.title}.mp3`;
-            let tags = {
-              title: item.title,
-              artist: item.artist,
-              album: item.album,
-              trackNumber: item.trackNumber,
-              comment: {
-                text: artistPage
+    function getTracks(albumArtworkBuffer) {
+      format.forEach(function (item) {
+        if (item)
+          axios({method: 'get', url: item.url, responseType: 'stream'})
+          .then(function (response) {
+            let write = fs.createWriteStream(`./${albumName}/${item.title}.mp3`);
+  
+            response.data.pipe(write);
+  
+            write.on('finish', () => {
+              let file = `./${albumName}/${item.title}.mp3`;
+              let tags = {
+                title: item.title,
+                artist: item.artist,
+                album: item.album,
+                trackNumber: item.trackNumber,
+                comment: {
+                  text: artistPage
+                },
+                image: {
+                  mime: "jpeg",
+                  type: {
+                    id: 3,
+                    name: "front cover"
+                  },
+                  imageBuffer: albumArtworkBuffer
+                }
               }
-            }
-            let success = NodeID3.write(tags, file);
-
-            if (success===true) {
-              console.log('saved: ', tags)
-            }
-            else {
-              console.log('error: error saving ID3 tag for ', tags)
-            }
-          });
-        })
-        .catch(err => console.log(err));
-    });
+              let success = NodeID3.write(tags, file);
+  
+              if (success===true) {
+                console.log('saved: ', tags.artist, tags.title)
+              }
+              else {
+                console.log('error: error saving ID3 tag for ', tags)
+              }
+            });
+          })
+          .catch(err => console.log(err));
+      });
+    }
 }).catch(err => console.log(err))
